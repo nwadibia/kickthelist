9e05e7a3b5d10a63594506738ea3ca12842caae4
“Emeka <enwadibia@gmail.com>
2015-07-06 15:36:40 -0700

    fixed associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_man :pins 
+  has_many :pins 
 end
